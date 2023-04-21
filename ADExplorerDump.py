e62bbd47318cef995d955840f04a547c1419b6ff
Phil Wilcox <phil.wilcox@cyberis.com>
2023-04-21 14:41:17 +0100

    Initial commit

---
+++ ADExplorerDump.py
@@ -0,0 +1,8 @@
+import json
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="ADExplorerDump.py",
+        description="Dumps selected data from ADExplorerSnapshot.py JSON output"
+    )
